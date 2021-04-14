b5fd77cea7a19405c3dcd32831bbff95b69579c5
gcusms <2436210442@qq.com>
2021-04-14 21:32:09 +0800

    像素update

--- test/main.cpp
+++ test/main.cpp
@@ -6,18 +6,20 @@ using namespace cv;
 
 int main()
 {
-    Mat src_img  = imread("/home/sms/tu/1.jpeg");
+    Mat src_img  = imread("/home/sms/tu/spaceman.jpg");
     resize(src_img,src_img,Size(400,400));
 
-    imshow("src_img ",src_img);
+
     int x, y, z,g;
-//    for(int i = 0 ; i < src_img.cols; ++i)
-//    {
-//        for(int j= 0 ; j< src_img.rows; ++j)
-//        {
-//           k = src_img.at<Vec3b>(i ,j )[2];
-//        }
-//    }
+    for(int i = 0 ; i < src_img.cols - 255 ; ++i)
+    {
+        for(int j= 0 ; j< src_img.rows  -255; ++j)
+        {
+            src_img.at<Vec3b>(i,j)[0] = 123;
+            src_img.at<Vec3b>(i,j)[1] = 32;
+             src_img.at<Vec3b>(i,j)[2] = 22;
+        }
+    }
 
     //rgb颜色通道显示
     x = src_img.at<Vec3b>(src_img.cols /2 ,src_img.rows/2)[0];
@@ -27,7 +29,7 @@ int main()
     cout<<"x ="<<x<<endl;
     cout<<"y  ="<<y<<endl;
     cout<<"z  ="<<z<<endl;
-
+       imshow("src_img ",src_img);
     //灰度图通道显示
     cvtColor(src_img,src_img,COLOR_RGB2GRAY);
     g = src_img.at<uchar>(src_img.cols /2 ,src_img.rows/2);
